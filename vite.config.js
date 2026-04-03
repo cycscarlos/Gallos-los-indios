ed921dd41075bf443b04e03d21b4bb52eae1b6fc
cycscarlos <cycs.ingenieria@gmail.com>
2026-04-03 12:19:25 -0400

    refactor: remove PWA support (vite-plugin-pwa, service worker)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   root: '.',
@@ -17,73 +16,7 @@ export default defineConfig({
     }
   },
 
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['images/**/*', 'css/**/*'],
-      manifest: {
-        name: 'Gallos Los Indios',
-        short_name: 'Los Indios',
-        description: 'Criadero de gallos de combate de alta calidad',
-        theme_color: '#050505',
-        background_color: '#050505',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'public/images/favicon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'public/images/favicon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'public/images/favicon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,png,jpg,jpeg,svg,ico,woff,woff2}'],
-        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'google-fonts-stylesheets'
-            }
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-webfonts',
-              expiration: {
-                maxEntries: 30,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              }
-            }
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [],
 
   build: {
     outDir: 'dist',
